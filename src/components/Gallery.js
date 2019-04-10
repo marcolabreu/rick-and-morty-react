4e5cd91a8aac1059c6f9f7b6022df8c28685fb86
Marco L. Abreu <marcolabreu@royalmail.top>
2019-04-10 22:45:32 +0200

    Test filtering

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react'
-import { Query } from 'react-apollo'
+import React, {Component} from 'react'
+import {Query} from 'react-apollo'
 import gql from 'graphql-tag'
 import Card from './Card'
 
 const GET_CHARACTERS = gql`
   {
-      characters {
-        info {
-          count
-          pages
-          next
-          prev
-        }
-        results {
-          id
-          name
-          status
-          species
-          gender
-          image
-        }
+    characters(page: 1, filter: { status: "alive", species: "alien" }) {
+      info {
+        count
+        pages
+        next
+        prev
       }
+      results {
+        id
+        name
+        status
+        species
+        gender
+        image
+      }
+    }
   }
 `
 
@@ -28,25 +28,25 @@ export default class Gallery extends Component {
   render() {
     return (
       <Query query={GET_CHARACTERS} errorPolicy="all">
-        {({loading, error, data}) =>
-          {
-            if (loading) return <div>Loading...</div>
-            if (error) return <div>Something is wrong... {error.graphQLErrors.map(({ message }, i) => (
-              <p key={i}>{message}</p>
-            ))}</div>
+        {({loading, error, data}) => {
+          if (loading) return <div>Loading...</div>
+          if (error) return <div>Something is wrong... {error.graphQLErrors.map(({message}, i) => (
+            <p key={i}>{message}</p>
+          ))}</div>
 
-            const info = data.characters.info
-            const characters = data.characters.results
+          const info = data.characters.info
+          const characters = data.characters.results
 
-            return (
-              <div className="Gallery">
-                {characters.map(character => <Card
-                  key={character.id}
-                  character={character}
-                />)}
-              </div>
-            )
-          }
+
+          return (
+            <div className="Gallery">
+              {characters.map(character => <Card
+                key={character.name}
+                character={character}
+              />)}
+            </div>
+          )
+        }
         }
       </Query>
     )
