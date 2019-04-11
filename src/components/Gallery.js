ecf75a182a5c491d3ebb77daae45402daa8e714e
Marco L Abreu <marcolabreu@users.noreply.github.comgit>
2019-04-11 09:55:03 +0200

    Add state to Gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,35 +4,48 @@ import Card from './Card'
 import {GET_CHARACTERS} from "./Queries";
 
 export default class Gallery extends Component {
+  state = {
+    page: null,
+    filter: {
+      status: "",
+      species: "",
+      gender: "",
+      name: ""
+    }
+  }
+
   render() {
     return (
-      <Query query={GET_CHARACTERS}
-             errorPolicy="all"
-             variables={{
-               page: null,
-               status: "alive",
-               species: "",
-               gender: "",
-               name: "rick",
-             }}
-      >
-        {({loading, error, data}) => {
-          if (error) return <div>Something is wrong...</div>
-          if (loading) return <div>Loading...</div>
+      <div>
+        <h2>Awesome Paginator</h2>
+        <Query query={GET_CHARACTERS}
+               errorPolicy="all"
+               variables={{
+                 page: this.state.page,
+                 status: this.state.filter.status,
+                 species: this.state.filter.species,
+                 gender: this.state.filter.gender,
+                 name: this.state.filter.name,
+               }}
+        >
+          {({loading, error, data}) => {
+            if (error) return <div>Something is wrong...</div>
+            if (loading) return <div>Loading...</div>
 
-          const info = data.characters.info
-          const characters = data.characters.results
+            const info = data.characters.info
+            const characters = data.characters.results
 
-          return (
-            <div className="Gallery">
-              {characters.map(character => <Card
-                key={character.name}
-                character={character}
-              />)}
-            </div>
-          )
-        }}
-      </Query>
+            return (
+              <div className="Gallery">
+                {characters.map(character => <Card
+                  key={character.name}
+                  character={character}
+                />)}
+              </div>
+            )
+          }}
+        </Query>
+      </div>
     )
   }
 }
